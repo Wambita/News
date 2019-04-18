7a548ab590ce5dc7ef0ee6e3c5cda6e30401a39a
Wambita Fana <wambitafana@gmail.com>
2019-04-18 15:23:59 +0300

    Generate an API key for website

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'WELCOME TO NEWS APP'
-    return render_template('index.html',message = message )
+    title = 'Home - Welcome to The best Online News Website'
+    return render_template('index.html', title = title)
 
-@app.route('/news/<int:news_id>')
-def movie(news_id):
+@app.route('/news/<news_id>')
+def news(news_id):
 
     '''
     View news page function that returns the news details page and its data
